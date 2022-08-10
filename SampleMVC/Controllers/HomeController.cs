c7a7111e84c7887919756f4ad3264d61834bb88a
Abel Palma <apalma@tsc.com.gt>
2022-08-10 17:27:03 -0600

    Add project files.

---
+++ SampleMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleMVC.Models;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using AdventureWorksNS.Data;    
+
+namespace SampleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AdventureWorksDB db;
+
+        public HomeController(ILogger<HomeController> logger, AdventureWorksDB injectedContext)
+        {
+            _logger = logger;
+            db = injectedContext;
+        }
+
+        [ResponseCache(Duration = 10, Location = ResponseCacheLocation.Any)]
+        public IActionResult Index()
+        {
+            _logger.LogError("This is a serious error");
+            _logger.LogWarning("This is a warning");
+            _logger.LogWarning("This is a second warning");
+            _logger.LogInformation("I am in the index method of HOME Controller");
+            HomeIndexViewModel model = new(
+                VisitorCount: (new Random()).Next(1, 1001),
+                Categories: db.ProductCategories.ToList(),
+                Products: db.Products.ToList()
+                );
+            return View(model);
+        }
+
+        [Route("private")]
+        [Authorize(Roles = "Administrators")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SampleMVC/Models/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AdventureWorksNS.Data;
+
+namespace SampleMVC.Models
+{
+    public record HomeIndexViewModel
+    (
+        int VisitorCount,
+        IList<ProductCategory> Categories,
+        IList<Product> Products
+    );
+}
